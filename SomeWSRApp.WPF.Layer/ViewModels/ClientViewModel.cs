042afbc0e0252517816ec7944b1d9f89b4e63073
MetalcoreLoli <38354462+MetalcoreLoli@users.noreply.github.com>
2021-04-26 01:46:27 +0300

    add some data to datagrid und go to sleep

--- SomeWSRApp.WPF.Layer/ViewModels/ClientViewModel.cs
+++ SomeWSRApp.WPF.Layer/ViewModels/ClientViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using SomeWSRApp.Domain.Layer.Entities;
+using SomeWSRApp.EF.Layer.Entities;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,5 +19,23 @@ namespace SomeWSRApp.WPF.Layer.ViewModels
             set => SetProperty(ref _selectedClient, value);
         }
         public ObservableCollection<ClientEntity> Clients { get; set; }
+
+        public ClientPageViewModel()
+        {
+            var load = Task.Factory.StartNew(() => 
+            {
+                using var context = new EntityContext();
+                context.Client.Load();
+                Clients = new ObservableCollection<ClientEntity>(
+                    context.Client.Local.Select(c => new ClientEntity 
+                    {
+                        Id = c.Id,
+                        Name = c.DataOfUser.FirstName,
+                        LastName = c.DataOfUser.LastName,
+                        Source = new SourceEntity(c.Source.Id, c.Source.Name)
+                    }));
+            });
+            load.Wait();
+        }
     }
 }
